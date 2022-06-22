e8265c917c58ddb5e259273a074e4edf126d2a0e
Shunzhe <git@mszmagic.com>
2022-06-22 13:51:32 +0900

    Initial Commit

---
+++ BackgroundTaskSwiftUIDemo/BackgroundTaskSwiftUIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BackgroundTaskSwiftUIDemoApp.swift
+//  BackgroundTaskSwiftUIDemo
+//
+//  Created by Shunzhe on 2022/06/22.
+//
+
+import SwiftUI
+
+@main
+struct BackgroundTaskSwiftUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BackgroundTaskSwiftUIDemo/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  BackgroundTaskSwiftUIDemo
+//
+//  Created by Shunzhe on 2022/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
